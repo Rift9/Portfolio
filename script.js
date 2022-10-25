8bcb63fb2c9910e7023fe7dc5d948322f339152b
Rift9 <udoalvin0@gmail.com>
2022-10-25 23:43:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let myFavoriteFruits = ["banana", "cherry", "mango",];
+
+myFavoriteFruits.push("pear");
+console.log(myFavoriteFruits);
+
+myFavoriteFruits.push("watermelon");
+console.log(myFavoriteFruits);
+
+myFavoriteFruits.push("banana");
+console.log(myFavoriteFruits);
+
+let theNewFruit = myFavoriteFruits.push();
+console.log(theNewFruit);
+console.log(myFavoriteFruits.length);
+
+//myFavoriteFruits.pop();
+console.log(myFavoriteFruits);
+console.log(myFavoriteFruits.length);
+
+let theLastFruit = myFavoriteFruits.pop();
+console.log(theLastFruit);
+console.log(myFavoriteFruits.length);
+
+console.log(myFavoriteFruits[4])
